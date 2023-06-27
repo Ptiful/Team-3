b4f004bc8aa83530a13d8c16d8f7fe6475302c5d
Ptiful <strazzu@gmail.com>
2023-06-27 09:40:29 +0200

    cleaning repo

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Accenture
+Accenture asked us to manipulate datas they sent so we could answer some questions for them as :
+- 'What categories/subcategories are projected to grow the fastest in the next 5 years?'
+- 'Within there Categories/Subcategories, what Channels are growing ?'
+- 'What is Dragonyte's market position within these fast-growing categories ?'
+- 'Provide predictions and explanation of the model used'
+- 'Give your recommendations to the Dargonyte Board'.
+
+## To run
+Run 'main.py' to clean all csvs files. 
+This will automatically move files from the data_old folder to data_new folder.
+
+### Tools
+Pandas, Tableau, Python 3.10 or higher.
+
+#### Contributor
+Melih Orhan Yilmaz
+Christos Raptis
+Grégoire Hupin
+Paul Strazzulla
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# Importing first csv and cleaning it, by dropiing and renaming
 df = pd.read_csv("data_old/Market_Sizes.csv")
 df = df.dropna()
 df = df.drop(
@@ -20,18 +21,20 @@ df = df.rename(columns={"RSP": "RSP_USD_Mnl"})
 df.loc[(df["RSP_USD_Mnl"] < df["Volume"]), "RSP_USD_Mnl"] = df["RSP_USD_Mnl"] * 1000
 df.to_csv("data_new/Market_Sizes.csv", index=False)
 
+
+# Importing second csv and cleaning it by droping, renaming
 df2 = pd.read_csv("data_old/Channel_Volume.csv")
 df2.dropna()
 df2["Volume"] = [float(str(i).replace(",", ".")) for i in df2["Volume"]]
-df2.loc[df["Unit"] == "million litres", "Volume"] = df2["Volume"] * 100
-df2 = df.drop(
+df2.loc[df2["Unit"] == "million litres", "Volume"] = df2["Volume"] * 100
+df2 = df2.drop(
     columns=["Category", "Unit", "Industry", "Edition", "Data_Type", "Year_date"]
 )
 df2 = df2.rename(columns={"Volume": "Volume_Kl", "Year_text": "Year"})
 
 df2.to_csv("data_new/Channel_Volume.csv", index=False)
 
-
+# Importing third csv and cleaning it by droping, renaming, changing type and replacing ',' by '.'
 df3 = pd.read_csv("data_old/Company_Share_GBO_unit.csv", sep=";")
 df3 = df3.dropna()
 df3["Volume"] = [float(str(i).replace(",", ".")) for i in df3["Volume"]]
@@ -50,13 +53,7 @@ df3 = df3.astype(
 ).round({"Location": 1, "Subcategory_ID": 1, "Hierarchy_Level": 1, "Year": 1})
 df3.to_csv("data_new/Company_Share_GBO_unit.csv", index=False)
 
+# Importing fourth csv and cleaning it by droping rows stating with ";"
 df4 = pd.read_csv("data_old/Locations.csv", delimiter=";")
 df4 = df.dropna(axis=1)
 df4.to_csv("data_new/Locations.csv", index=False)
-
-df5 = pd.read_csv("data_new/Company_Share_GBO_unit.csv")
-df6 = pd.read_csv("data_new/Subcategories.csv")
-df6 = df6.merge(df5, left_on="id", right_on="Subcategory_ID")
-df6 = df6[["id", "Category", "Name", "Subcategory_ID", "Hierarchy_Level"]]
-df6.drop_duplicates(subset=["Subcategory_ID"], inplace=True)
-df6.to_csv("data_new/Subcategories2.csv")
